883653f0dd7a0c99b555cbdee265f7c5aa959ba0
KHW <hwkim@13mile.co.kr>
2019-08-06 11:18:32 +0900

    response Type to Rxjava

--- app/src/main/java/kr/evalon/usingopenapi/ApiUnsplash.kt
+++ app/src/main/java/kr/evalon/usingopenapi/ApiUnsplash.kt
@@ -1,18 +1,23 @@
 package kr.evalon.usingopenapi
 
+import io.reactivex.Single
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ApiUnsplash {
 
     @GET("/me")
-    fun loadProfile():Any
+    fun loadProfile(): Single<Any>
 
     companion object {
 
         fun create():ApiUnsplash{
             return Retrofit.Builder()
                 .baseUrl("https://api.unsplash.com/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ApiUnsplash::class.java)
         }

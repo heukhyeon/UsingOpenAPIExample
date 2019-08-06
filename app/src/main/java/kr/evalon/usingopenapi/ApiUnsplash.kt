e8af7e0166b7648f06725ff7f52d1149b8826fa1
KHW <hwkim@13mile.co.kr>
2019-08-06 12:18:29 +0900

    remove loadProfile

--- app/src/main/java/kr/evalon/usingopenapi/ApiUnsplash.kt
+++ app/src/main/java/kr/evalon/usingopenapi/ApiUnsplash.kt
@@ -1,6 +1,9 @@
 package kr.evalon.usingopenapi
 
 import io.reactivex.Single
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,8 +11,8 @@ import retrofit2.http.GET
 
 interface ApiUnsplash {
 
-    @GET("/me")
-    fun loadProfile(): Single<Any>
+    @GET("/photos")
+    fun loadPhoto():Single<Any>
 
     companion object {
 
@@ -18,8 +21,30 @@ interface ApiUnsplash {
                 .baseUrl("https://api.unsplash.com/")
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(createClient())
                 .build()
                 .create(ApiUnsplash::class.java)
+        }
+
+        private fun createClient():OkHttpClient{
+            return OkHttpClient.Builder()
+
+                .addInterceptor {
+                    val url = it.request().url.newBuilder()
+                        .addQueryParameter("client_id","92448cb1d51196350ff106bc897517a384b8eba59f26e83c188f177ee0b23c1d")
+                        .build()
+
+                    val request = it.request().newBuilder()
+                        .url(url)
+                        .addHeader("Accept-Version","v1")
+                        .build()
+
+                    it.proceed(request)
+                }
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+                .build()
         }
     }
 }
--- app/src/test/java/kr/evalon/usingopenapi/ApiTest.kt
+++ app/src/test/java/kr/evalon/usingopenapi/ApiTest.kt
@@ -4,15 +4,19 @@ import io.reactivex.observers.TestObserver
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLog
 
 @RunWith(RobolectricTestRunner::class)
 class ApiTest {
 
     @Test
-    fun loadProfile(){
+    fun loadPhoto(){
+        ShadowLog.stream = System.out
+
         val testObserver = TestObserver<Any>()
         ApiService.unSplash()
-            .loadProfile()
+            .loadPhoto()
             .subscribe(testObserver)
 
         testObserver.await()
